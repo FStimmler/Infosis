b67bc9d88d4369c0910fa1d90a53c0d1d522ccbb
Silverado42 <43814714+Silverado42@users.noreply.github.com>
2018-11-28 11:52:41 -0300

    Add files via upload

--- Fisica/src/Fisica_main.java
+++ Fisica/src/Fisica_main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class Fisica_main {
+	static int xi=0,xf=0,vi=0,vf=0,a=0,t=0;
 
 	public static void main (String[] args) {
+		@SuppressWarnings("resource")
 		Scanner reader = new Scanner(System.in);
 		String incog1="0";
 		String incog2="0";
@@ -17,55 +19,160 @@ public class Fisica_main {
 			System.out.println("y?: ");
 			incog2 = reader.next();
 		}
-		System.out.println(incog1);
-		if(incog1.equals("xf")) {
-			System.out.println("xf");
-		}
+
 
 
 		if(!(incog2.equals("xf")||incog1.equals("xf"))) {
 			System.out.println("Posicion inicial: ");
-			int xi = reader.nextInt();
+			xi = reader.nextInt();
 		}if(!(incog2.equals("xf")||incog1.equals("xf"))) {
 			System.out.println("Posicion final: ");
-			int xf = reader.nextInt();
-		}if(incog2!="vi"||incog1!="vi") {
+			xf = reader.nextInt();
+		}if(!(incog2.equals("vi")||incog1.equals("vi"))) {
 			System.out.println("Velocidad inicial: ");
-			int vi = reader.nextInt();
-		}if(incog2!="vf"||incog1!="vf") {
+			vi = reader.nextInt();
+		}if(!(incog2.equals("vf")||incog1.equals("vf"))) {
 			System.out.println("Velocidad final: ");
-			int vf = reader.nextInt();
-		}if(incog2!="t"||incog1!="t") {
+			vf = reader.nextInt();
+		}if(!(incog2.equals("t")||incog1.equals("t"))) {
 			System.out.println("Tiempo: ");
-			int t = reader.nextInt();
-		}if(incog2!="a"||incog1!="a") {
+			t = reader.nextInt();
+		}if(!(incog2.equals("a")||incog1.equals("a"))) {
 			System.out.println("Aceleracion: ");
-			int a = reader.nextInt();
+			a = reader.nextInt();
 		}
 
-
-		/*
-		System.out.println("Posicion inicial: ");
-		int xi = reader.nextInt();
-		System.out.println("Posicion final: ");
-		int xf = reader.nextInt();
-		System.out.println("Velocidad inicial: ");
-		int vi = reader.nextInt();
-		System.out.println("Velocidad final: ");
-		int vf = reader.nextInt();
-		//System.out.println("Tiempo: ");
-		//int t = reader.nextInt();
-		System.out.println("Aceleracion: ");
-		int a = reader.nextInt();
-
-		int t =(vf-vi)/a;
-
-		System.out.println(incog1);
-	*/
+		if (c_incog==1) {
+			switch (incog1) {
+			case "vf":
+				vf=vi+a*t;
+				mostrar("vf");
+				break;
+			case "vi":
+				vi=vf-a*t;
+				mostrar("vi");
+				break;
+			case "xi":
+				xi=xf-vi*t-(a/2)*(t*t);
+				mostrar("xi");
+				break;
+			case "xf":
+				xf=xi+vi*t+(a/2)*(t*t);
+				mostrar("xf");
+				break;
+			case "a":
+				a=(vf-vi)/t;		
+				mostrar("a");
+				break;
+			case "t":
+				t=(vf-vi)/a;		
+				mostrar("t");
+				break;
+			}
+		}
+		else {
+			switch (incog1) {
+			case "xf":
+				switch(incog2) {
+				case "xi":
+					System.out.println("Imposible de resolver");
+					break;
+				case "vi":
+					vi=vf-a*t;
+					xf=xi+vi*t+(a/2)*(t*t);
+					mostrar("vi");
+					mostrar("xf");
+					break;
+				case "vf":
+					xf=xi+vi*t+(a/2)*(t*t);
+					vf=vi+a*t;
+					mostrar("vf");
+					mostrar("xf");
+					break;
+				case "t":
+					t=(vf-vi)/a;
+					xf=xi+vi*t+(a/2)*(t*t);
+					mostrar("xf");
+					mostrar("t");
+					break;
+				case "a":
+					a=(vf-vi)/t;
+					xf=xi+vi*t+(a/2)*(t*t);
+					mostrar("xf");
+					mostrar("t");
+					break;
+				}
+				break;
+			case "vi":
+				switch(incog2) {
+				case "vf":
+					vi=(xf-xi-(a/2)*(t*t))/t;
+					vf=vi+a*t;
+					mostrar("vi");
+					mostrar("vf");
+					break;
+				case "xi":
+					vi=vf-a*t;
+					xi=xf-vi*t-(a/2)*(t*t);
+					mostrar("vi");
+					mostrar("xi");
+					break;
+				case "xf":
+					vi=vf-a*t;
+					xf=xi+vi*t+(a/2)*(t*t);
+					mostrar("vi");
+					mostrar("xf");
+				case "t":
+					System.out.println("Muy dificil: ");
+					break;
+				case "a":
+					System.out.println("Muy dificil: ");
+					break;	
+				}
+				break;
+			case "vf":
+				switch(incog2) {
+				case "vi":
+					vi=(xf-xi-(a/2)*(t*t))/t;
+					vf=vi+a*t;
+					mostrar("vi");
+					mostrar("vf");
+					break;
+				case "xi":
+					break;
+				}
+				break;
+			}
+		}
 	}
 
-	void datos() {
+	static void mostrar(String string) {
+		switch(string) {
+		case "vi":
+			System.out.println("Velocidad inicial: ");
+			System.out.println(vi);
+			break;
+		case "vf":	
+			System.out.println("Velocidad final: ");
+			System.out.println(vf);
+			break;
+		case "xi":
+			System.out.println("Posicion inicial: ");
+			System.out.println(xi);
+			break;
+		case "xf":
+			System.out.println("Posicion final: ");
+			System.out.println(xf);
+			break;
+		case "t":
+			System.out.println("Tiempo: ");
+			System.out.println(t);
+			break;
+		case "a":
+			System.out.println("Aceleracion: ");
+			System.out.println(a);
+			break;
 
+		}
 	}
-
 }
